3093a03846da4f5bc7e29e898c998a06a9a612ae
allw <allanwiese@gmail.com>
2018-08-04 22:08:17 -0300

    Componente Sorteio

---
+++ sorteios/src/components/Sorteio.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sorteio">
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+        Sorteios
+        {{ number }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sorteio',
+  data() {
+    return {
+      number: 12
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
