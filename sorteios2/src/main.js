553577da392c0a74b7b4dd242360e2613dbb60b3
allw <allanwiese@gmail.com>
2018-08-15 20:30:57 -0300

    tentativas com pacotes de terceiros

--- sorteios2/src/main.js
+++ sorteios2/src/main.js
@@ -1,7 +1,8 @@
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
 import Vue from 'vue'
 import App from './App.vue'
+import RandomJs from 'randomorg-js'
 
 new Vue({
   el: '#app',
